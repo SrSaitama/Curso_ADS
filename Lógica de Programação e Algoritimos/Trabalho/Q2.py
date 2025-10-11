0e41a7b6a4a0acf4ac4b01ab831259bf17cd8957
Marcelo Augusto <marceloaugustomoura@proton.me>
2025-10-11 14:23:49 -0300

    update Q2.py

--- Lógica de Programação e Algoritimos/Trabalho/Q2.py
+++ Lógica de Programação e Algoritimos/Trabalho/Q2.py
@@ -1,105 +1,66 @@
-print('-'*10, ' Bem-vindo a Pizzaria do Bruno Eliakim ', '-'*10)
-print('-'*25, 'Cardápio', '-'*25)
-print('-'*60)
+print("-" * 10, " Bem-vindo a Pizzaria do Bruno Eliakim ", "-" * 10)
+print("-" * 25, "Cardápio", "-" * 25)
+print("-" * 60)
 
 dados = [
     ["P", "R$ 30.00", "R$ 34.00"],
     ["M", "R$ 45.00", "R$ 48.00"],
-    ["G", "R$ 60.00", "R$ 66.00"]
+    ["G", "R$ 60.00", "R$ 66.00"],
 ]
 
-print(f"---| {'Tamanho':<10} | {'Pizza Salgada (PS)':<10} | {'Pizza Doce (PD)':<10} |---")
+print(
+    f"---| {'Tamanho':<10} | {'Pizza Salgada (PS)':<10} | {'Pizza Doce (PD)':<10} |---"
+)
 
 for linha in dados:
     tamanho, salgada, doce = linha
     print(f"---| {tamanho:<10} | {salgada:<18} | {doce:<15} |---")
 
-print("-" * 60) # Linha inferior da tabela
+print("-" * 60)
+
+valor_total = 0
+step = 1
 
 while True:
-    sabor = input("Entre com o sabor desejado (PS/PD): ").upper().strip()
-    if sabor in ("PD", "PS"):
+
+    if step == 1:
+        sabor = input("Entre com o sabor desejado (PS/PD): ").upper().strip()
+        if sabor not in ("PD", "PS"):
+            print("Sabor inválida, tente novamente.")
+            continue
+        step += 1
+
+    if step == 2:
+        tamanho = input("Entre com o tamanho desejado (P/M/G): ").upper().strip()
+        if tamanho not in ("P", "M", "G"):
+            print("Tamanho inválida, tente novamente.")
+            continue
+        step += 1
+
+    if sabor == "PD":
+        if tamanho == "P":
+            valor_total += 34
+        elif tamanho == "M":
+            valor_total += 48
+        else:
+            valor_total = 66
+    else:
+        if tamanho == "P":
+            valor_total += 30
+        elif tamanho == "M":
+            valor_total += 45
+        else:
+            valor_total += 60
+
+    print(f"O valor total ate agora é: {valor_total}")
+    resposta = input("Deseja pedir mais alguma coisa? (sim/não): ")
+
+    if resposta.lower() == "não":
+        print(f"total a pagar: {valor_total}")
         break
+    elif resposta.lower() == "sim":
+        step = 1
+        continue
     else:
-        print("Sabor inválida, tente novamente.")
+        print("Resposta inválida. Por favor, responda com 'sim' ou 'não'.")
 
-while True:
-    tamanho = input("Entre com o tamanho desejado (P/M/G): ").upper().strip()
-    if tamanho in ("P", "M", "G"):
-        break
-    else:
-        print("Tamanho inválida, tente novamente.")
-
-print(f"{sabor}")
-print(f"{tamanho}")
-
-if(sabor == "PD"):
-    if(tamanho == "P"):
-        valor = 34
-    elif(tamanho == "M"):
-        valor = 48
-    else:
-        valor = 66
-else:
-    if(tamanho == "P"):
-        valor = 30
-    elif(tamanho == "M"):
-        valor = 45
-    else:
-        valor = 60
-
-print(f"{valor}")
-
-
-
-
-
-
-
-
-
-
-""""
-opc = str(input("Deseja mais alguma coisa? (S/N) ")).upper().strip()
-
-if (opc == "S"):
-
-        while True:
-            sabor = input("Entre com o sabor desejado (PS/PD): ").upper().strip()
-            if sabor in ("PD", "PS"):
-                if(sabor == "PD"):
-                    if(tamanho == "P"):
-                        valor = 34
-                    elif(tamanho == "M"):
-                        valor = 48
-                    else:
-                        valor = 66
-                else:
-                    if(tamanho == "P"):
-                        valor = 30
-                    elif(tamanho == "M"):
-                        valor = 45
-                    else:
-                        valor = 60
-
-            else:
-                print("Sabor inválida, tente novamente.")
-
-            while True:
-                tamanho = input("Entre com o tamanho desejado (P/M/G): ").upper().strip()
-                if tamanho in ("P", "M", "G"):
-                    continue
-                else:
-                    print("Tamanho inválida, tente novamente.")
-
-            print(f"{sabor}")
-            print(f"{tamanho}")
-
-            
-            print(f"{valor}")
-elif(opc == "N"):
-    print(f"O valor total a ser pago: R$ {valor:.2f}")
-else:
-    print("Opção inválida, tente novamente.")
-    
-"""
