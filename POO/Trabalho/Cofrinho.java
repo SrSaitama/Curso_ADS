045b953dbe69f0c4409371eb0a870636d3b57451
Senhor Holmes <saitama10k1@gmail.com>
2025-12-08 22:19:25 -0300

    trabalho

--- POO/Trabalho/Cofrinho.java
+++ POO/Trabalho/Cofrinho.java
@@ -3,31 +3,29 @@ package Curso_ADS.POO.Trabalho;
 import java.util.ArrayList;
 
 public class Cofrinho {
-	
+
 	private ArrayList<Moeda> listaMoedas = new ArrayList<>();
-	
-	
+
 	public void adicionar(Moeda m) {
 		listaMoedas.add(m);
-		
-	}	
+
+	}
+
 	public void remover(Moeda m) {
 		listaMoedas.remove(m);
-		
-	}	
-	
+	}
+
 	public void listagemMoedas() {
-		for(Moeda m : listaMoedas) {
+		for (Moeda m : listaMoedas) {
 			m.info();
 		}
-		
-	}	
-	
+	}
+
 	public void totalConvertido() {
-		double total=0;
-		for(Moeda m : listaMoedas) {
+		double total = 0;
+		for (Moeda m : listaMoedas) {
 			total += m.converter();
 		}
-		System.out.println("O totsl convertido para real: " + total);
+		System.out.println("O total convertido para real: " + total);
 	}
 }
