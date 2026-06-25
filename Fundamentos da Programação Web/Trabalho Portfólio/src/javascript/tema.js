e38c78596d6512b69d9da52e86b560152d94fdd2
Senhor Holmes <saitama10k1@gmail.com>
2026-06-25 15:41:15 -0300

    Arquivo de Tema

---
+++ Fundamentos da Programação Web/Trabalho Portfólio/src/javascript/tema.js
@@ -0,0 +1,26 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const butTema = document.getElementById('btn-tema');
+
+    //VERIFICA SE O USUARIO JÁ ESCOLHEU O TEMA ESCURO,
+    //O localStorage vai guardar a decisão do usuario. 
+    if(localStorage.getItem('tema-escolhido') == 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+
+    //Verificar se o usuario apertou o botão
+    if(butTema) {
+        butTema.addEventListener('click', function(){
+            document.body.classList.toggle('dark-theme');
+
+            if(document.body.classList.contains('dark-theme')){
+                localStorage.setItem('tema-escolhido', 'dark');
+            
+            } else {
+                localStorage.setItem('tema-escolhido', 'light')
+            }
+        });
+    }
+
+});
